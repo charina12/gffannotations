fb270e89185f1fae70c9c65cef4fd105697cc96b
charina12 <50924237+charina12@users.noreply.github.com>
2025-02-12 16:58:51 -0500

    Add files via upload

---
+++ PROKKAsplit.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# path to .csv file with one column named "annotations"
+x = "/Users/charlottemanvell/MAG/kBase/GTDB-Tk/D62_bin2gff.csv"
+# path to write csv
+out = "/Users/charlottemanvell/MAG/kBase/GTDB-Tk/D62_bin2.csv"
+
+df = pd.read_csv(x)
+
+# create function to split row's string into a dictionary
+def split_row_to_dict(row):
+    key_value_pairs = row.split(';') #split input into factors, separated by ;
+    return {kv.split('=')[0]: kv.split('=')[1] for kv in key_value_pairs} #create columns with before = as colname and after = as value
+
+
+# create new dataframe by applying defined function to column of interest
+anno = pd.DataFrame(df['annotation'].apply(split_row_to_dict).tolist())
+# join with original data, keeps annotation column to reference
+new_df = df.join(anno)
+# write csv
+new_df.to_csv(out)
